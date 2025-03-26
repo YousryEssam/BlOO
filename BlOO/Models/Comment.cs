0523f2ab82f017153bef56fffbb5db6afcb3e430
Developer <Developer@MARLY>
2025-03-26 18:09:17 +0200

    Improvements

---
+++ BlOO/Models/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlOO.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "text")]
+        public string Content { get; set; }
+        [Required]
+        public DateTime CommentDate { get; set; } = DateTime.Now;
+        public int LikeCount { get; set; } = 0;
+        public int PostId { get; set; }
+        public int UserId { get; set; }
+        //[ForeignKey("PostId")]
+        //public virtual Post? post { get; set; }
+        //[ForeignKey("UserId")]
+        //public virtual User? user { get; set; }
+    }
+}
---
+++ BlOO/Repositories/RepostRepository.cs
@@ -0,0 +1,42 @@
+﻿using BlOO.Models;
+
+namespace BlOO.Repositories
+{
+    public class RepostRepository : IRepostRepository
+    {
+        public void Delete(Repost entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Repost> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Repost GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Repost entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Repost entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

ce8d9f8e8028dd8eccf92f5a722a5b5c68024dc7
YousryEssam <youssry.essam2111@gmail.com>
2025-03-29 20:34:16 +0200

    Profile Page V1.1

--- BlOO/ViewModels/ProfileViewModel.cs
+++ BlOO/ViewModels/ProfileViewModel.cs
@@ -1,5 +1,4 @@
-﻿using BlOO.Models;
-namespace BlOO.ViewModels
+﻿namespace BlOO.ViewModels
 {
     public class ProfileViewModel
     {
@@ -8,8 +7,8 @@ namespace BlOO.ViewModels
         public int PostCount { get; set; }
         public int FollowersCount { get; set; }
         public int FollowingCount { get; set; }
-        public string FirstName { get; set; } = "Yousry";
-        public string LastName { get; set; } = "Essam";
+        public string FirstName { get; set; } = "First Name";
+        public string LastName { get; set; } = "Last Name";
         public string UserName { get; set; } = "User_Name";
         public string ProfileImageUrl { get; set; } = "/assets/profile-pictures/default-user.jpg";
         public string CoverImageUrl { get; set; } = "/assets/profile-covers/default-cover.jpg";

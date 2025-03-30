3db897978b5c94284177275908621e962b4ea6a8
beshoyatef200000@gmail.com <beshoyatef200000@gmail.com>
2025-03-30 15:34:33 +0200

    Login&Register

---
+++ BlOO/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlOO.ViewModels
+{
+    public class RegisterViewModel
+    {
+        public string UserName { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; }
+
+    }
+}

a13884c4a8e8f7f95786398f8a94056bc85e9345
NourhanMMourise <magednourhan66@gmail.com>
2025-03-30 14:41:03 +0200

    post section was added

---
+++ BlOO/Controllers/PostController.cs
@@ -0,0 +1,24 @@
+﻿using BlOO.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlOO.Controllers
+{
+    public class PostController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+
+
+        public IActionResult Post()
+        {
+
+            PostViewModel postViewModel = new PostViewModel();
+            return View(postViewModel);
+        }
+
+
+    }
+}

9c0d4634836b6a69f53effb83f25eab464c74209
MarlyMonsef <marlymonsef21@gmail.com>
2025-04-01 20:29:59 +0200

    Add Chat Page

---
+++ BlOO/ViewModels/MessageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlOO.ViewModels
+{
+    public class MessageViewModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = "Madonna";
+        public string LastName { get; set; } = "Hany";
+        public string ProfileImageUrl { get; set; } = "/assets/icons/user.png";
+        [Required]
+        public string Content { get; set; } = "How are you ?";
+        public bool MessageSeen { get; set; } = false;
+        public DateTime SendingDate { get; set; } = DateTime.UtcNow;
+    }
+}
